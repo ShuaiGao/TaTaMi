31846511c0f925c1789ef02f1de8b8f82bdb7b1c
7yao_Gao <597449675@qq.com>
2016-10-10 21:42:58 +0800

    add camera control script.

---
+++ TaFang/Assets/Scripts/PathNode.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathNode : MonoBehaviour {
+
+    //父路点
+    public PathNode m_parent;
+    //子路点
+    public PathNode m_next;
+
+    //设置子节点
+    public void SetNext(PathNode node)
+    {
+        if(m_next != null)
+            m_next.m_parent = null;
+        m_next = node;
+        node.m_parent = this;
+    }
+
+    //显示路点图标
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(this.transform.position, "Node.tif");
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
